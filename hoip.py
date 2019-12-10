f9e58efbaf44fed07f1f39bd9ccd3654ae8bc2dd
Jingshen <js_gao@pku.edu.cn>
2019-12-10 16:16:09 +0800

    seperate combinations with unknown band gap

--- hoip.py
+++ hoip.py
@@ -2,5 +2,6 @@ import element_feature as ef
 import combination_generator as cg
 
 cwd = 'D:\\PycharmProjects\\HOIP_bandgap_prediction\\data\\'
-ef.element_feature(cwd)
-cg.combination_generator(cwd)
+#ef.element_feature(cwd)
+#cg.combination_generator(cwd)
+cg.unknown_combination_seperator(cwd)
