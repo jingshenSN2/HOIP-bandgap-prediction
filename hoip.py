a1158a5cacaf8f897779c34fc3763448d3c50e58
Marius <1700011714@pku.edu.cn>
2019-12-10 20:03:30 +0800

    changed notations

--- hoip.py
+++ hoip.py
@@ -8,4 +8,5 @@ cwd = 'D:\\PycharmProjects\\HOIP_bandgap_prediction\\data\\'
 #cg.combination_generator(cwd)
 #cg.unknown_combination_seperator(cwd)
 X_train, X_test, y_train, y_test, predict_X = pre.preprocessing(cwd)
+print(X_train)
 g.gbr(X_train, X_test, y_train, y_test)
--- preprocessing.py
+++ preprocessing.py
@@ -3,16 +3,16 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def preprocessing(cwd):
-    reg = MinMaxScaler()
+    scaler = MinMaxScaler()
     trates = pd.read_csv(cwd+'HOIP-30.csv',header=0)
     predic = pd.read_csv(cwd+'unknown_comb.csv',header=0)
     trates_y = trates['bandgap-PBE(eV)']
     trates.drop(['A-site','B-site','X-site','bandgap-PBE(eV)'], axis=1, inplace=True)
     predic.drop(['A-site','B-site','X-site','hash'], axis=1, inplace=True)
-    dfall = trates.append(predic)
-    dfall_mm = reg.fit_transform(dfall)
-    trates_X = dfall_mm[0:203]
-    predict_X = dfall_mm[203:-1]
+    df_all = trates.append(predic)
+    df_all_mm = scaler.fit_transform(df_all)
+    trates_X = df_all_mm[0:203]
+    predict_X = df_all_mm[203:-1]
     X_train, X_test, y_train, y_test = train_test_split(trates_X, trates_y, test_size=0.20, random_state=1)
     return X_train, X_test, y_train, y_test, predict_X
 
