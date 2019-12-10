7c76af224338ef365672b4413fa0923913d92a9f
GJS <js_gao@pku.edu.cn>
2019-12-10 19:53:42 +0800

    gbr v1

---
+++ gbr.py
@@ -0,0 +1,7 @@
+from sklearn.ensemble import GradientBoostingRegressor as GBR
+
+def gbr(X_train, X_test, y_train, y_test):
+    reg = GBR(random_state = 1)
+    reg.fit(X_train,y_train)
+    print(reg.score(X_test,y_test))
+    print(reg.feature_importances_)
--- hoip.py
+++ hoip.py
@@ -1,7 +1,11 @@
 import element_feature as ef
 import combination_generator as cg
+import preprocessing as pre
+import gbr as g
 
 cwd = 'D:\\PycharmProjects\\HOIP_bandgap_prediction\\data\\'
 #ef.element_feature(cwd)
 #cg.combination_generator(cwd)
 #cg.unknown_combination_seperator(cwd)
+X_train, X_test, y_train, y_test, predict_X = pre.preprocessing(cwd)
+g.gbr(X_train, X_test, y_train, y_test)
