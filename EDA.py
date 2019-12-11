7520a1dfb7a3ca0020019bbeb5ab40d789528149
Jingshen <js_gao@pku.edu.cn>
2019-12-11 20:30:47 +0800

    eda v1

---
+++ EDA.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+
+def __boxplot(cwd, data, name):
+    plt.figure(figsize=(18, 12))
+    for i in range(30):
+        plt.subplot(5, 6, i + 1)
+        data.iloc[:, i:i + 1].boxplot()
+    plt.subplots_adjust(hspace=0.3, wspace=0.3)
+    plt.savefig(cwd + name)
+
+def __density(cwd, data, name):
+    plt.figure(figsize=(18, 12))
+    for i in range(30):
+        plt.ylabel(None)
+        plt.subplot(5, 6, i + 1)
+        if i == 29:
+            continue
+        else:
+            data.iloc[:, i].plot(kind='kde')
+    plt.subplots_adjust(hspace=0.3, wspace=0.3)
+    plt.savefig(cwd + name)
+
+def raw_data_describe(cwd):
+    data = pd.read_csv(cwd+'all_combination.csv',header=0)
+    data.drop(['A-site','B-site','X-site'], axis=1, inplace=True)
+    data.describe(include=[np.number]).to_csv(cwd+'eda\\raw_describe.csv')
+    __boxplot(cwd, data, 'eda\\raw_boxplot.png')
+    data.drop(['X_f-electron'], axis=1, inplace=True)
+    __density(cwd, data, 'eda\\raw_density.png')
+
+def pre_processing_data_describe(cwd):
+    scaler = MinMaxScaler()
+    data = pd.read_csv(cwd+'all_combination.csv',header=0)
+    data.drop(['A-site','B-site','X-site'], axis=1, inplace=True)
+    npdata = scaler.fit_transform(data)
+    data = pd.DataFrame(npdata,columns= data.columns)
+    data.describe(include=[np.number]).to_csv(cwd+'eda\\pre_processing_describe.csv')
+    __boxplot(cwd, data, 'eda\\pre_processing_boxplot.png')
+    data.drop(['X_f-electron'], axis=1, inplace=True)
+    __density(cwd, data, 'eda\\pre_processing_density.png')
+
+
--- hoip.py
+++ hoip.py
@@ -2,6 +2,7 @@ import element_feature as ef
 import combination_generator as cg
 import preprocessing as pre
 import feature_selection as fs
+import EDA as e
 import gbr as g
 
 data_directory = 'D:\\PycharmProjects\\HOIP_bandgap_prediction\\data\\'
@@ -9,8 +10,13 @@ model_directory = 'D:\\PycharmProjects\\HOIP_bandgap_prediction\\model\\'
 #ef.element_feature(data_directory)
 #cg.combination_generator(data_directory)
 #cg.unknown_combination_seperator(data_directory)
-X_train, X_test, y_train, y_test, predict_X, features = pre.preprocessing(data_directory)
-feature_selection_gbr_reg_list = fs.feature_selector_gbr(X_train, X_test, y_train, y_test, features, model_directory)
+
+e.raw_data_describe(data_directory)
+e.pre_processing_data_describe(data_directory)
+
+#X_train, X_test, y_train, y_test, predict_X, features = pre.preprocessing(data_directory)
+
+#feature_selection_gbr_reg_list = fs.feature_selector_gbr(X_train, X_test, y_train, y_test, features, model_directory)
 #feature_selection_mlp_reg_list = fs.feature_selector_mlp(X_train, X_test, y_train, y_test, features, model_directory)
 #X_train, X_test, y_train, y_test, predict_X, features = pre.preprocessing(data_directory)
 #feature_selection_pca_reg_list = fs.feature_selector_pca(X_train, X_test, y_train, y_test, features, model_directory)
