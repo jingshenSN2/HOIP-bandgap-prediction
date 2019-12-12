a6fd8bd1725d867516b0298640fa0bb172e1507f
Jingshen <js_gao@pku.edu.cn>
2019-12-12 15:07:53 +0800

    Multiple choices of model for feature selection.

--- feature_selection.py
+++ feature_selection.py
@@ -1,51 +1,46 @@
 from sklearn.ensemble import GradientBoostingRegressor as GBR
+from sklearn.ensemble import RandomForestRegressor as RFR
+from sklearn.ensemble import AdaBoostRegressor as ABR
+from sklearn.ensemble import ExtraTreesRegressor as ETR
 from sklearn.decomposition import PCA
 from sklearn.decomposition import KernelPCA
 from sklearn.neural_network import MLPRegressor
 import pandas as pd
 import numpy as np
+import preprocessing as pre
 
-def __gbr_test(X_train, X_test, y_train, y_test):
-    reg = GBR(random_state=1)
+def __ensemble_test(type, X_train, X_test, y_train, y_test):
+    if type.lower() == 'gbr':
+        reg = GBR(n_estimators = 100, random_state=1)
+    elif type.lower() == 'rfr':
+        reg = RFR(n_estimators = 100, random_state=1)
+    elif type.lower() == 'abr':
+        reg = ABR(n_estimators = 100, random_state=1)
+    elif type.lower() == 'etr':
+        reg = ETR(n_estimators = 100, random_state=1)
     reg.fit(X_train, y_train)
     return reg, reg.score(X_test, y_test), reg.feature_importances_
 
-def feature_selector_gbr(X_tr, X_te, y_tr, y_te, feature, cwd):
+def feature_selector_ensemble(type, cwdd, cwdm):
+    X_train, X_test, y_train, y_test, predict_X, features = pre.preprocessing(cwdd)
     reg_list = []
-    X_train, X_test, y_train, y_test = X_tr, X_te, y_tr, y_te
-    local_feature = feature
+    local_feature = features
     df = pd.DataFrame(columns=['number','reg_score','adj. r**2','feature_list'])
     while len(local_feature) > 1:
-        reg, score, weight = __gbr_test(X_train, X_test, y_train, y_test)
+        reg, score, weight = __ensemble_test(type, X_train, X_test, y_train, y_test)
         reg_list.append(reg)
-        df = df.append(pd.DataFrame([[len(local_feature),score,1-(1-score*score)*(40)/(40-len(local_feature)),np.array(local_feature)]],columns=df.columns), ignore_index=True)
+        df = df.append(pd.DataFrame([[len(local_feature),score,1-(1-score*score)*(40)/(40-len(local_feature)),', '.join(local_feature)]],columns=df.columns), ignore_index=True)
         low = np.argmin(weight)
         del local_feature[low]
         X_train = np.delete(X_train, low, axis=1)
         X_test = np.delete(X_test, low, axis=1)
-    df.to_csv(cwd+'feature_selection_gbr.csv', index=False)
+    df.to_csv(cwdm+'feature_selection_%s.csv'%type, index=False)
     return reg_list
 
 def __mlp_test(X_train, X_test, y_train, y_test):
     reg = MLPRegressor(hidden_layer_sizes=(20,),max_iter=10000,random_state=1)
     reg.fit(X_train, y_train)
     return reg, reg.score(X_test, y_test)
-
-def feature_selector_mlp(X_tr, X_te, y_tr, y_te, feature, cwd):
-    reg_list = []
-    X_train, X_test, y_train, y_test = X_tr, X_te, y_tr, y_te
-    local_feature = feature
-    df = pd.DataFrame(columns=['number','reg_score','feature_list'])
-    while len(local_feature) > 1:
-        reg, score, weight = __mlp_test(X_train, X_test, y_train, y_test)
-        reg_list.append(reg)
-        low = np.argmin(weight)
-        del local_feature[low]
-        df = df.append(pd.DataFrame([[len(local_feature),score,np.array(local_feature)]],columns=df.columns), ignore_index=True)
-        X_train = np.delete(X_train, low, axis=1)
-        X_test = np.delete(X_test, low, axis=1)
-    df.to_csv(cwd+'feature_selection_mlp.csv', index=False)
-    return reg_list
 
 def __pca_test(dem, X_tr, X_te, y_train, y_test):
     reg = KernelPCA(kernel='linear',n_components=dem, random_state=1)
--- hoip.py
+++ hoip.py
@@ -9,21 +9,24 @@ data_directory = 'D:\\PycharmProjects\\HOIP_bandgap_prediction\\data\\'
 model_directory = 'D:\\PycharmProjects\\HOIP_bandgap_prediction\\model\\'
 
 #初始数据集去重
-ef.raw_drop_duplicates(data_directory, silent=0)
+#ef.raw_drop_duplicates(data_directory, silent=0)
 
 #分离ABX离子特征
-ef.element_feature(data_directory, silent=0)
+#ef.element_feature(data_directory, silent=0)
 
 #生成特征组合的全集和补集
-cg.combination_generator(data_directory, silent=0)
-cg.unknown_combination_seperator(data_directory, silent=0)
+#cg.combination_generator(data_directory, silent=0)
+#cg.unknown_combination_seperator(data_directory, silent=0)
 
-e.raw_data_describe(data_directory)
-e.pre_processing_data_describe(data_directory)
+#e.raw_data_describe(data_directory)
+#e.pre_processing_data_describe(data_directory)
 
-X_train, X_test, y_train, y_test, predict_X, features = pre.preprocessing(data_directory)
+#X_train, X_test, y_train, y_test, predict_X, features = pre.preprocessing(data_directory)
 
-feature_selection_gbr_reg_list = fs.feature_selector_gbr(X_train, X_test, y_train, y_test, features, model_directory)
+feature_selection_gbr_reg_list = fs.feature_selector_ensemble('gbr', data_directory, model_directory)
+feature_selection_rfr_reg_list = fs.feature_selector_ensemble('rfr', data_directory, model_directory)
+feature_selection_abr_reg_list = fs.feature_selector_ensemble('abr', data_directory, model_directory)
+feature_selection_etr_reg_list = fs.feature_selector_ensemble('etr', data_directory, model_directory)
 #feature_selection_mlp_reg_list = fs.feature_selector_mlp(X_train, X_test, y_train, y_test, features, model_directory)
 #X_train, X_test, y_train, y_test, predict_X, features = pre.preprocessing(data_directory)
 #feature_selection_pca_reg_list = fs.feature_selector_pca(X_train, X_test, y_train, y_test, features, model_directory)
