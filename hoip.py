b30090d3ffab0387be18d93c9b9fe6ba069249d4
Jingshen <js_gao@pku.edu.cn>
2019-12-14 11:43:18 +0800

    Multiple choices of model for feature selection.

--- hoip.py
+++ hoip.py
@@ -23,14 +23,24 @@ model_directory = 'D:\\PycharmProjects\\HOIP_bandgap_prediction\\model\\'
 #e.pre_processing_data_describe(data_directory)
 
 #不同方法做每次舍弃最差的特征筛选
-feature_selection_gbr_reg_list = fs.feature_selector_ensemble('gbr', data_directory, model_directory)
-feature_selection_rfr_reg_list = fs.feature_selector_ensemble('rfr', data_directory, model_directory)
-feature_selection_abr_reg_list = fs.feature_selector_ensemble('abr', data_directory, model_directory)
-feature_selection_etr_reg_list = fs.feature_selector_ensemble('etr', data_directory, model_directory)
+#feature_selection_gbr_reg_list = fs.feature_selector_ensemble('gbr', data_directory, model_directory)
+#feature_selection_rfr_reg_list = fs.feature_selector_ensemble('rfr', data_directory, model_directory)
+#feature_selection_abr_reg_list = fs.feature_selector_ensemble('abr', data_directory, model_directory)
+#feature_selection_etr_reg_list = fs.feature_selector_ensemble('etr', data_directory, model_directory)
 
 #考察PCA降维的影响
 #feature_selection_mlp_reg_list = fs.feature_selector_mlp(X_train, X_test, y_train, y_test, features, model_directory)
 
 
+#Gaussian Boosting R
+#g.gbr(cwdm, features)
 
-#g.gbr(X_train, X_test, y_train, y_test, features)
+#Kernel Ridge R
+
+#Support Vector R
+
+#Gaussian Process R
+
+#Decision Tree R
+
+#MLP R
